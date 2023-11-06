691569a7a2d1a7d7cb614cbf58bdc5063e775afe
trickyshikari <jarik.zp.tm@gmail.com>
2023-11-06 17:31:59 +0300

    test "get"

--- test/class.js
+++ test/class.js
@@ -12,4 +12,14 @@ describe('constructor', function(){
         var mtrx = Array(a);
         expect(new Mtrx(a)).to.deep.equal(mtrx)
     })
+
+    it('get', function(){
+        const a = [
+            [1,2,3],
+            [4,5,6]
+        ]        
+        var mtrx = new Mtrx(a);
+
+        expect(mtrx.get(0,0)).to.equal(1)
+    })
 })
